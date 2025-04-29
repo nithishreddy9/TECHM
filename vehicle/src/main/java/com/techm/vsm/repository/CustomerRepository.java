88fb59383a69fac67ceef69d0b006429f3457884
nithishreddy9 <151392106+nithishreddy9@users.noreply.github.com>
2025-04-29 10:37:20 +0530

    Delete vehicle directory

--- vehicle/src/main/java/com/techm/vsm/repository/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.techm.vsm.repository;
-
-import com.techm.vsm.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
